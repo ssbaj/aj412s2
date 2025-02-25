accc88a56836bb6b26e2188b6114083e4b2b34e6
ssbaj <ssb9018@ajou.ac.kr>
2025-02-25 15:38:00 +0900

    first commit

--- R/find_outlier.R
+++ R/find_outlier.R
@@ -10,11 +10,11 @@ cat("    ", '\n')
 
 cat(" \033[1;36m# car패키지의 influencePlot() --------------- \033[0m ", '\n'   )
 print( car::influencePlot(Result) )
-
 cat("    ", '\n')
-
-cat(" \033[1;36m# 우측의 Plot을 참고하세요 \033[0m ", '\n'   )
-
+cat(" \033[1;36m# 우측의 Plot은 influencePlot에서 제공한 Plot입니다\033[0m ", '\n'   )
+cat("    ", '\n')
+cat(" \033[1;36m# 다음은 Plot을 사용해 극단치를 찾는 방법입니다 \033[0m ", '\n'   )
+plot( Result, which=c(1:5) )  # 극단치 찾아내기
 cat("    ", '\n')
 
 }
