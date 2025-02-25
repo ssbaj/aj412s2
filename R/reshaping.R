3453cd5ef6b335ade942d71ac0dffcfdeaba796c
ssbaj <ssb9018@ajou.ac.kr>
2025-02-25 16:43:09 +0900

    first commit

--- R/reshaping.R
+++ R/reshaping.R
@@ -13,6 +13,7 @@ reshaping<-function(df, commoncol, repeating){
 		return(cat("      ") ) }
 
 
+df<-as.data.frame(df)
 
 vnames<-c('v1','v2','v3','v4','v5','v6','v7',
           'v8','v9','v10','v11','v12','v13','v14','v15','v16','v17',
