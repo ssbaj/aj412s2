66e814802b64209f5103e0e445ded689991cd5de
ssbaj <ssb9018@ajou.ac.kr>
2025-08-05 19:02:10 +0900

    first commit

--- R/geodistance22.R
+++ R/geodistance22.R
@@ -1,6 +1,6 @@
 geodistance22<-function(CENTER, TARGET){
   if (base::missing(CENTER)) {
-    cat(" \033[1;34m# llibrary(aj412s2); library(geosphere); library(dplyr) ", '\n')
+    cat(" \033[1;34m# library(aj412s2); library(geosphere); library(dplyr) ", '\n')
     cat(" \033[1;34m# 위도(lat)와 경도(long)로 CENTER와 TARGET의 거리 측정 --- ", '\n')
     cat(" \033[1;34m# CENTER 데이터셋과 TARGET 데이터셋에 각각 lat와 long 변수가 있어야 함 \033[0m ", '\n')
     return(cat(" \033[1;34m# geodistance22(CENTER, TARGET) \033[0m ") ) }
