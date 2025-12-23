58d87ef24edea2236ec07061c53bd11e2b6559ea
ssbaj <ssb9018@ajou.ac.kr>
2025-12-23 15:35:10 +0900

    first commit

--- R/mlogit_prob.R
+++ R/mlogit_prob.R
@@ -3,9 +3,9 @@ mlogit_prob <- function(mLogitResult, my_input=NULL) {
 if (base::missing(mLogitResult)) {
 cat(" \033[1;32m# 명령문 예제 ---------- \033[0m", '\n' )
 cat(" \033[1;32m new_data <- data.frame( \033[0m", '\n' )
-cat(" \033[1;32m gender = rep('남성', 61), \033[0m", '\n' )
-cat(" \033[1;32m age = rep(30, 61), \033[0m", '\n' )
-cat(" \033[1;32m income = seq(2000, 8000, 100) \033[0m", '\n' )
+cat(" \033[1;32m gender = rep('남성', 10), \033[0m", '\n' )
+cat(" \033[1;32m age = rep(30, 10), \033[0m", '\n' )
+cat(" \033[1;32m income = seq(2000, 8000, (8000-2000)/(10-1)) \033[0m", '\n' )
 cat(" \033[1;32m ) \033[0m", '\n' )
 return( cat("\033[1;33m 명령문: mlogit_prob(multinom분석결과, new_data) \033[0m", '\n') )
 }
