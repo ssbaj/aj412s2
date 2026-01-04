0f5f6b737e4c5c730a2b64cdd766d6f007c52e5d
ssbaj <ssb9018@ajou.ac.kr>
2026-01-04 12:00:08 +0900

    first commit

--- R/addcomma.R
+++ R/addcomma.R
@@ -1,5 +1,11 @@
 addcomma <- function(input_string) {
 
+if (base::missing(input_string)) {
+     cat("\033[1;32m  # 명령문: x<-'1 2 3 4 5' \033[0m ", '\n')
+     cat("\033[1;32m  # 명령문: x<-addcomma(x) \033[0m ", '\n')
+     cat("\033[1;32m  # 명령문: x<-addquote(x) \033[0m ", '\n')
+     return(cat("      ",'\n'))  }
+
 no_spaces <- gsub(" ", ",", input_string)
 no_spaces <- gsub("\t", ",", no_spaces)
 no_spaces <- gsub(" ,", ",", no_spaces)
--- R/addquote.R
+++ R/addquote.R
@@ -1,9 +1,10 @@
 addquote <- function(input_string) {
 
 if (base::missing(input_string)) {
-cat(" \033[1;36m# Examples ---------- \033[0m", '\n' )
-cat(" \033[1;36m variables <- 'v1, v2, v3' \033[0m", '\n' )
-return( cat(" \033[1;36m addquote(variables) \033[0m", '\n' )) }
+     cat("\033[1;32m  # 명령문: x<-'1 2 3 4 5' \033[0m ", '\n')
+     cat("\033[1;32m  # 명령문: x<-addcomma(x) \033[0m ", '\n')
+     cat("\033[1;32m  # 명령문: x<-addquote(x) \033[0m ", '\n')
+     return(cat("      ",'\n'))  }
 
 words <- strsplit(input_string, ",")[[1]]
 words <- trimws(words) 
